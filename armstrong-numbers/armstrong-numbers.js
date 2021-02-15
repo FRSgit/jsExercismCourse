fdd72f3682874fc6ace1899053e9caad75a2c14a
filip232 <filip.wesolowski@onet.eu>
2021-02-15 20:48:16 +0100

    Squashed commit of the following:
    commit 2ff51ae1a3231ffc372811b642cccd6537b84059
    Author: filip232 <filip.wesolowski@onet.eu>
    Date:   Mon Feb 15 20:47:57 2021 +0100
    
        mob next [ci-skip]
    
    commit ae270ff2090a3097564550533a483250498c2f8a
    Author: Dodonito <dominik14081999@wp.pl>
    Date:   Mon Feb 15 20:38:09 2021 +0100
    
        mob next [ci-skip]
    
    commit b491e8813bb889d96e748e9041289e070a4e4caf
    Author: Maciej <radymmaciej@gmail.com>
    Date:   Mon Feb 15 20:10:02 2021 +0100
    
        mob next [ci-skip]

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -3,6 +3,11 @@
 // convenience to get you started writing code faster.
 //
 
-export const isArmstrongNumber = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isArmstrongNumber = number => {
+    const numberString = number.toString();
+    let sum = 0;
+    for (let i = 0, len = numberString.length; i < len; i++) {
+        sum += numberString[i] ** len;
+    }
+    return sum === number;
 };
