a035e47f456ee6544648996aa6df697fe1a82348
Jakub Freisler <jakubfreisler@phrase.com>
2021-03-11 21:15:32 +0100

    chore: start vue-fizzbuzz

---
+++ vue/fizzbuzz/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <input type="text" v-model="name" />
+  {{ name }}
+  <HelloWorld msg="Welcome to Your Vue.js + "/>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    return {
+      name: ref('cokolwiek'),
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+$elo: 60px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: $elo;
+}
+</style>
