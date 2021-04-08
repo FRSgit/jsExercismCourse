be4959a090b89c0b5986b49d9878b8b680353f0e
Maciej <radymmaciej@gmail.com>
2021-04-08 22:25:50 +0200

    part 1 minesweeper

---
+++ vue/minesweeper/src/components/Game.vue
@@ -0,0 +1,41 @@
+<template>
+  <Map :map="map"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Map from './Map.vue';
+
+export default defineComponent({
+  name: 'Game',
+  components: {
+      Map,
+  },
+  setup(){
+      const map = new Array(8)
+        .fill(0)
+        .map(() => new Array(8).fill(' '));
+      const temp = new Array(64)
+        .fill(0)
+        .map((_val, index) => index);
+      
+      for(let i = 0; i < 10; ++i){
+        const index_temp = Math.floor(Math.random() * temp.length);
+        const pos = temp[index_temp];
+        const y = Math.floor(pos / 8);
+        const x = pos % 8;
+        map[y][x] = '*';
+        temp.splice(index_temp, 1);
+      }
+      
+      return {
+          map: map.map((val) => val.join('')),
+      }
+  }
+});
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
---
+++ vue/minesweeper/src/components/Map.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="map">
+    <div class="map__row" v-for="(row, y) in map" :key="y">
+      <span class="map__cell" v-for="(col, x) in row" :key="x">
+        {{ col==='*'? '💣' : col }}
+      </span>
+    </div>
+  </div>
+  
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'Map',
+  props: {
+    map: Array as PropType<string[]>,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.map{
+  background-color: #AAA;
+  &__cell{
+    display: inline-block;
+    height: 3ch;
+    width: 3ch;
+    border: thin solid black;
+    vertical-align: middle;
+  }
+}
+</style>
